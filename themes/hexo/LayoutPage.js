74a596e619c61167214ff9391e3d24d35e2519a7
kkobikim <kkobi.kim@gbike.io>
2023-03-15 09:22:07 +0900

    e

--- themes/hexo/LayoutPage.js
+++ themes/hexo/LayoutPage.js
@@ -3,8 +3,9 @@ import CategoryList from './components/CategoryList'
 import LayoutBase from './LayoutBase'
 
 export const LayoutPage = (props) => {
+  const {  category, categories } = props
   return <LayoutBase {...props}>
-    <CategoryList currentCategory={category} categories={categories} />ddddd
+    <CategoryList currentCategory={category} categories={categories} />
       <BlogPostListPage {...props}/>
   </LayoutBase>
 }
