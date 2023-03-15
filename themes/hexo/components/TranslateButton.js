16f02b31b981d53b49503de13750c3b0e177948e
kkobikim <kkobi.kim@gbike.io>
2023-03-15 12:20:02 +0900

    ㅋ

--- themes/hexo/components/TranslateButton.js
+++ themes/hexo/components/TranslateButton.js
@@ -1,56 +1,40 @@
-import { useEffect, useState } from 'react';
+import React, { useState } from 'react';
+import axios from 'axios';
 
-const TranslateButton = ({ notionText }) => {
-  const [browserLanguage, setBrowserLanguage] = useState(null);
-  const client_id = 'WQdE4NOH_Gt6GpbfLQ9E';
-  const client_secret = 'f6grx1UfPg';
-  const supportedLanguages = ['en', 'zh-CN', 'zh-TW', 'es', 'fr', 'vi', 'th', 'id'];
+const TranslateButton = ({ content, setContent }) => {
+  const [loading, setLoading] = useState(false);
 
-  useEffect(() => {
-    if (window && window.navigator) {
-      let lang = window.navigator.language.slice(0, 2);
-      if (lang === 'ko' || !supportedLanguages.includes(lang)) {
-        lang = 'en';
-      }
-      setBrowserLanguage(lang);
-    }
-  }, []);
+  const translateContent = async () => {
+    setLoading(true);
+    const browserLanguage = navigator.language || navigator.userLanguage;
+    const targetLanguage = browserLanguage.substr(0, 2);
 
-  const handleTranslate = async () => {
-    const api_url = './api/translate';
-    const requestOptions = {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify({
-        'source': 'ko',
-        'target': browserLanguage,
-        'text': notionText,
-      }),
-    };
-  
     try {
-      const response = await fetch(api_url, requestOptions);
-      if (response.ok) {
-        const data = await response.json();
-        const translatedText = data.message.result.translatedText;
-        // notion-text 클래스를 가진 모든 요소에 번역된 텍스트 적용
-        document.querySelectorAll('.notion-text').forEach((element, index) => {
-          element.innerText = translatedText[index];
-        });
-      } else {
-        console.error('Error:', response.status);
+      const response = await axios.post(
+        'https://openapi.naver.com/v1/papago/n2mt',
+        { source: 'ko', target: targetLanguage, text: content },
+        {
+          headers: {
+            'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8',
+            'X-Naver-Client-Id': 'WQdE4NOH_Gt6GpbfLQ9E',
+            'X-Naver-Client-Secret': 'f6grx1UfPg',
+          },
+        }
+      );
+
+      if (response.data.message.result) {
+        setContent(response.data.message.result.translatedText);
       }
     } catch (error) {
-      console.error('Error:', error);
+      console.error(error);
+    } finally {
+      setLoading(false);
     }
   };
-  
 
   return (
-    <button onClick={handleTranslate}>
-      {browserLanguage ? `번역 (${browserLanguage})` : '번역'}
+    <button onClick={translateContent} disabled={loading}>
+      번역
     </button>
   );
 };
