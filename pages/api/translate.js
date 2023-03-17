45643eee75f19720d67f487a8c31e67b5aa4bae8
kkobikim <kkobi.kim@gbike.io>
2023-03-18 01:08:55 +0900

    3

--- pages/api/translate.js
+++ pages/api/translate.js
@@ -17,7 +17,7 @@ export default async function handler(req, res) {
     const completion = await openai.createChatCompletion({
       model: 'gpt-3.5-turbo',
       messages: [
-        { role: 'system', content: `지쿠와 gcoo는 GCOO , 지쿠터와 gcooter는 Gcooter, 지바이크와 gbike는 Gbike 로 보여줘, Translate the text to ${targetLanguage}.` },
+        { role: 'system', content: `지쿠와 gcoo 와 GCOO 는 GCOO , 지쿠터와 gcooter는 Gcooter, 지바이크와 gbike는 Gbike 로 보여줘, Translate the text to ${targetLanguage}.` },
         { role: 'user', content: text },
       ],
     });
