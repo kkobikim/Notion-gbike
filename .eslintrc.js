5d6cd0b27bb34dd5adb1fffdb84158301573975a
kkobikim <kkobi.kim@gbike.io>
2023-04-12 20:04:05 +0900

    aa

--- .eslintrc.js
+++ .eslintrc.js
@@ -25,15 +25,9 @@ module.exports = {
       version: 'detect'
     }
   },
-  rules: {
-    'react/prop-types': 'off',
-    'space-before-function-paren': 0,
-    'react-hooks/rules-of-hooks': 'error', // Checks rules of Hooks
-    'quotes': 'off',
-    'semi': 'off',
-    'comma-dangle': 'off',
-    // 모든 규칙을 비활성화하려면 아래 라인을 사용하세요.
-    'off'
+  rules: rules: {
+    // 모든 규칙을 비활성화하려면 이 라인을 사용하세요.
+    "off"
   },
   globals: {
     React: true
