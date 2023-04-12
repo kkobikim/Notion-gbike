cf23a14e64eac166140393fcbbd44544206299c9
kkobikim <kkobi.kim@gbike.io>
2023-04-12 20:57:10 +0900

    -

--- components/Busuanzi.js
+++ components/Busuanzi.js
@@ -1,6 +1,6 @@
-import busuanzi from '@/lib/busuanzi'
+import busuanzi from '/lib/busuanzi'
 import { useRouter } from 'next/router'
-import { useGlobal } from '@/lib/global'
+import { useGlobal } from '/lib/global'
 // import { useRouter } from 'next/router'
 import React from 'react'
 
